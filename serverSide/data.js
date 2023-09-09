ece58fc01de6efebce0be02e442dca5886dfbc43
Abdullah Al Zaman <abdullahalzaman@Abdullahs-MacBook-Pro.local>
2023-09-09 20:43:14 +0600

    one more change

---
+++ serverSide/data.js
@@ -0,0 +1,21 @@
+const sales = [
+    {
+        "product": "T-Shirt",
+        "quantity": 1,
+        "price": 300,
+        "date": Date.now()
+    },
+    {
+        "product": "Jeans Pant",
+        "quantity": 2,
+        "price": 2099,
+        "date": Date.now()
+    },
+    {
+        "product": "IPHONE 13",
+        "quantity": 1,
+        "price": 122549,
+        "date": Date.now()
+    }
+]
+module.exports = { sales } 
